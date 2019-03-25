075489a4b259d2e9a7ac4bcbbef99b340d01b102
Katsumi Kusumi <katsumi.kusumi.ph@gmail.com>
2019-03-26 00:17:23 +0900

    Change font.

--- app/src/main/java/com/test/TestApp/MainActivity.kt
+++ app/src/main/java/com/test/TestApp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.test.TestApp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,13 +8,22 @@ import com.test.TestApp.ListViewTest.ListViewTestActivity
 import com.test.TestApp.PokeCardList.PokeCardListActivity
 import com.test.TestApp.PrefectureListView.PrefectureActivity
 import com.test.TestApp.RecyclerViewTest.RecyclerViewTestActivity
+import com.test.TestApp.TextInputTest.TextInputTestActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CalligraphyConfig.initDefault(
+                CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        )
 
         setupWidget()
     }
@@ -38,5 +48,14 @@ class MainActivity: AppCompatActivity() {
             val intent = Intent(this, PokeCardListActivity::class.java)
             startActivity(intent)
         }
+
+        btnTextInputTest.setOnClickListener {
+            val intent =  Intent(this, TextInputTestActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
     }
 }
---
+++ app/src/main/java/com/test/TestApp/TextInputTest/TextInputTestActivity.kt
@@ -0,0 +1,13 @@
+package com.test.TestApp.TextInputTest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.test.TestApp.R
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class TextInputTestActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_textinputtest)
+    }
+}
